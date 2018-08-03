d66203b2f9cba4b5fe697911a2bd71c9231beba2
Jeffrey Lovell <eyekno3w@hotmail.com>
2018-08-02 20:44:31 -0700

    Updated Posts page to show the list of post titles. Needs more work, but currently does connect to API and also renders the list of Posts. More styling and functionality to be added.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -7,13 +8,35 @@ class PostsIndex extends Component {
     console.log('Loading Posts Index');
     this.props.fetchPosts();
   }
+
+  renderPosts(){
+    // have to use lodash's map function, since posts is an object
+    return _.map(this.props.posts, post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render(){
+    // console.log('Current properties in Posts:', this.props.posts);  // Log properties
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state){
+  return{
+    posts: state.posts
+  };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
